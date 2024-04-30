7688a81cba03fa2f5e21186c1ad8c3d57585c381
Gabe <gabe@truesdell.me>
2024-04-30 18:54:15 -0400

    changed some text in "Win" class

--- win_views.py
+++ win_views.py
@@ -95,7 +95,7 @@ class Win(arcade.View):
             anchor_x="center",
         )
         arcade.draw_text(
-            f"You've finished the game and collected {self.objectives} out of 9 gems, good job",
+            f"You've finished the game and collected all 9 gems, great job.",
                 SCREEN_WIDTH / 2,
                 SCREEN_HEIGHT / 2,
                 arcade.color.WHITE,
